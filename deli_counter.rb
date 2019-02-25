907ba295f378f71106e126a9bfca89ef3ee1645e
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-25 22:36:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ end
 def nos_serving(line)
   if line.length == 0 
   if line.emty?
-    puts "there is nobody waiting to be served"
+    puts "There is nobody waiting to be served"
   else
   puts "Currently serving #{line.first}"
   line.shift
