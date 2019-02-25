473efc89e572d95bbf506f4905ad820d86fa69d9
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-25 22:17:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@
 def line 
   katz_deli = []
 end
-def take_a_number(array,line)
-  
+def take_a_number(name,line)
+  name.push
   puts "Welcome, #{name}.You are number #{line} in line"
 end
 def nos_serving(line)
